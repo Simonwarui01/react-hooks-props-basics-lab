ebf1ee17d2dfa9cccd658b1efb3922cf2695fbd0
Simon <simonwarui93@gmail.com>
2025-05-30 16:27:54 +0300

    assignment

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const user = {
+  name: "Simon",
+  city: "Nairobi",
+  bio: "I love building cool web apps using React!",
+  image: "https://via.placeholder.com/150",
+  github: "https://github.com/yourusername",
+  linkedin: "https://linkedin.com/in/yourusername"
+};
+
+export default user;
